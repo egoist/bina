0159cb17bf31692c1dcb6a672c7234749a4e5b02
EGOIST <0x142857@gmail.com>
2021-12-03 17:09:46 +0800

    add umami

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          {process.env.NODE_ENV === "production" && (
+            <script
+              async
+              defer
+              data-website-id="ce03835b-fcb3-4afd-9db2-73867fc28261"
+              src="https://umami.egoist.sh/umami.js"
+            ></script>
+          )}
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
