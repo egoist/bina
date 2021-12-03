677e5502b5818e2de656b9021df61203fc21d755
EGOIST <0x142857@gmail.com>
2021-12-03 17:17:55 +0800

    tweak code blocks

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export const CodeBlock: React.FC<{ code: string }> = ({ code }) => {
   return (
-    <pre className="text-yellow-500 font-mono my-5">
+    <pre className="text-yellow-500 font-mono my-8 overflow-auto border border-gray-800 p-5">
       <code>{code}</code>
     </pre>
   )
