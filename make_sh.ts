c09a1e7c2a01c7b913f57fce35b20c7f3cee6edc
Siddharth <ahoy@barelyhuman.dev>
2021-12-04 12:06:03 +0530

    add a realpath polyfill for sh
    either this or we let people know that they need to add the needed
    tool on their OS for this to work

--- make_sh.ts
+++ make_sh.ts
@@ -232,6 +232,18 @@ export const makeInstallerScript = ({
     echo "\${TMPDIR}"
   }
 
+  realpath() {
+    OURPWD=$PWD
+    cd "$(dirname "$1")"
+    LINK=$(readlink "$(basename "$1")")
+    while [ "$LINK" ]; do
+      cd "$(dirname "$LINK")"
+      LINK=$(readlink "$(basename "$1")")
+    done
+    REALPATH="$PWD/$(basename "$1")"
+    cd "$OURPWD"
+    echo "$REALPATH"
+  }
   
   start() {
     repo="${repo.owner}/${repo.name}"
