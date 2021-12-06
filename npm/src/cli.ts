0ba4d4cce1e8c5fb0549540b90194437dbb6d3f9
EGOIST <0x142857@gmail.com>
2021-12-06 13:50:12 +0800

    Add npm package

---
+++ npm/src/cli.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import { cac } from "cac"
+import { execSync } from "child_process"
+
+let cli = cac("bina")
+
+cli
+  .command("[repo]", "Install a binary from a repository")
+  .option("--token <gh_token>", "Set a GitHub token to access private repos")
+  .option(
+    "-d, --install-dir <dir>",
+    "Set the installation directory (default: /usr/local/bin)"
+  )
+  .option("-n, --name <name>", "Set the binary name (default: repo name)")
+  .action((repo, flags) => {
+    if (!repo) return cli.outputHelp()
+
+    const search = new URLSearchParams(
+      [
+        ["token", flags.token],
+        ["dir", flags.installDir],
+        ["name", flags.name],
+        ["debug", process.env.BINA_DEBUG ? "true" : ""],
+      ].filter((v) => Boolean(v[1]))
+    ).toString()
+
+    execSync(
+      `curl -fsSL "http://localhost:3000/${repo}${
+        search ? `?${search}` : ""
+      })" | sh`,
+      {
+        stdio: "inherit",
+      }
+    )
+  })
+
+cli.parse()
